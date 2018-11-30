a642876a07becc21645d6df877651f0267d60be6
srtekMAC <srtekmac@srtekmacs-macbook-air.local>
2018-11-30 12:09:51 +0530

    vc2

---
+++ storyboard/B2ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  B2ViewController.swift
+//  storyboard
+//
+//  Created by srtekMAC on 30/11/18.
+//  Copyright © 2018 srtekMAC. All rights reserved.
+//
+
+import UIKit
+
+class B2ViewController: UIViewController {
+
+    @IBOutlet weak var vc2lbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("vc2")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
