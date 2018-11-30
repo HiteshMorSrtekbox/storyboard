bbad641b76ba5e50932293beb1dc09d9ef5dcee1
srtekMAC <srtekmac@srtekmacs-macbook-air.local>
2018-11-30 12:04:47 +0530

    B1 commit

---
+++ storyboard/VC1ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  VC1ViewController.swift
+//  storyboard
+//
+//  Created by srtekMAC on 30/11/18.
+//  Copyright © 2018 srtekMAC. All rights reserved.
+//
+
+import UIKit
+
+class VC1ViewController: UIViewController {
+
+    @IBOutlet weak var VC1lABEL: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("VC1")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
